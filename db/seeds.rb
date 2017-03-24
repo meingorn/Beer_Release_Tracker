86eb4475b73be05a527d8edc93c4257bb27d7e56
github username <meingorn@gmail.com>
2017-03-24 14:52:23 -0400

    beer stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Beer.create!(name: "Heady Topper", style: "Imperial IPA", abv: 8.0, description:"Customers are urged to drink directly from the Heady Topper can. Heady Topper is a double India Pale Ale brewed by The Alchemist in Stowe, VT. It is unfiltered and contains 8% ABV, with hints of citrus flavors. Unpasteurized, it is kept refrigerated by authorized retailers until point of sale.", brewery_id: 1, account_id: nil )
