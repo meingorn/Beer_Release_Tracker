cc48c53877520072d208b38847b81966aa06f8a0
Alexandra Roth <alroth8@gmail.com>
2017-03-24 16:50:14 -0400

    nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get root 'static#index'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
-  resources :breweries
+  resources :breweries do
+  	resources :beers, only: [:index, :show]
+  end
   resources :beers
   resources :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
